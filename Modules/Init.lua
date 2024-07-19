981aa98d3508aa0937a3ef5038160cfc81ea8262
Constellation <168604061+AquaMan4094@users.noreply.github.com>
2024-07-19 07:53:00 -0400

    Update Init.lua

--- Modules/Init.lua
+++ Modules/Init.lua
@@ -106129,17 +106129,15 @@ local LuauCompile = {
 	}
 }
 
-local Bytecode = LuauCompile.luau_compile("print'hi'", 1,1,1)
-local luau_execute = Fiu.luau_load(Bytecode, getfenv())
-luau_execute()
 
 function LoadBuffer(Str)
 	local Bytecode = LuauCompile.luau_compile(Str, 1,1,1)
 	local luau_execute = Fiu.luau_load(Bytecode, getfenv())
-	return setfenv(luau_execute(), getfenv())	
 end
 
-LoadBuffer("print('Hello from loadstring!')")()
+local Bytecode = LuauCompile.luau_compile("print'hi' LoadBuffer('print'ls')()", 1,1,1)
+local luau_execute = Fiu.luau_load(Bytecode, getfenv())
+luau_execute()
 
 end)
 
