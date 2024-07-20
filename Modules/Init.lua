b4a576c4d3316cfc77b543ea7f63a930c47bd33d
Constellation <168604061+AquaMan4094@users.noreply.github.com>
2024-07-20 15:24:21 -0400

    Update Init.lua

--- Modules/Init.lua
+++ Modules/Init.lua
@@ -324,7 +324,7 @@ local function luau_deserialize(bytecode, luau_settings)
 	local luauVersion = readByte()
 	local typesVersion = 0
 	if luauVersion == 0 then
-		print(tostring(stream))
+		print(readString)
 		error("the provided bytecode is an error message",0)
 	elseif luauVersion < 3 or luauVersion > 6 then
 		error("the version of the provided bytecode is unsupported",0)
